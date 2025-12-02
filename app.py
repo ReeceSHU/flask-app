5526f2f6795fac8f64b0925f623b3688286bd7c8
ReeceSHU <Reece.Chan@student.shu.ac.uk>
2025-12-02 10:06:45 +0000

    AllowCrossOrigin

--- app.py
+++ app.py
@@ -9,5 +9,12 @@ app.register_blueprint(main_blueprint)
 def inject_globals():
     return {'current_year': datetime.now().year}
 
+@app.after_request
+def after_request(response):
+  response.headers.add('Access-Control-Allow-Origin', '*')
+  response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+  response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+  return response
+
 if __name__ == '__main__':
     app.run(debug=True)
